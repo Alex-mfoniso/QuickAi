576625c9c0c20fda2fb5f49817cb1cee5c65b71e
Alex-mfoniso <alexandermfoniso18@gmail.com>
2025-07-22 22:10:46 +0100

    done with the dashboard

--- client/src/pages/Dashboad.jsx
+++ client/src/pages/Dashboad.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { dummyCreationData } from "../assets/assets";
-import { Sparkle } from "lucide-react";
+import { Gem, Sparkle } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../components/CreationItem";
 
 const Dashboad = () => {
   const [creation, setCreation] = useState([]);
@@ -21,10 +23,34 @@ const Dashboad = () => {
             <p>Total Creation</p>
             <h2>{creation.length}</h2>
           </div>
-          <div>
-            <Sparkle className="w-5 text-white"/>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#358f2] to-[#0bb0d7] text-white flex justify-center items-center">
+            <Sparkle className="w-5 text-white" />
           </div>
         </div>
+
+        {/* active plan card */}
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p>Active Plan</p>
+            <h2>
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#ff61c5] to-[#9e53ee] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
+          </div>
+        </div>
+      </div>
+
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {
+          creation.map((item) => 
+            <CreationItem key={item.id}  item={item} />
+          )
+        }
       </div>
     </div>
   );
