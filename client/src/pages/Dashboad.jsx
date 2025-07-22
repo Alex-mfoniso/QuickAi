ff62e7c39652741ca334f06733dda7c1c15ce75f
Alex-mfoniso <alexandermfoniso18@gmail.com>
2025-07-22 21:00:25 +0100

    Update Dashboad.jsx

--- client/src/pages/Dashboad.jsx
+++ client/src/pages/Dashboad.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Sparkle } from "lucide-react";
 
 const Dashboad = () => {
-  return (
-    <div>Dashboad</div>
-  )
-}
+  const [creation, setCreation] = useState([]);
 
-export default Dashboad
+  const getDashboardData = async () => {
+    setCreation(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+  return (
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        {/* total creation card */}
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p>Total Creation</p>
+            <h2>{creation.length}</h2>
+          </div>
+          <div>
+            <Sparkle className="w-5 text-white"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboad;
